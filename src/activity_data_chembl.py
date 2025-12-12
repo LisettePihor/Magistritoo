356ff9c5916e00ebea85c72b46c60afcad301ff2
Lisette <pihor.lisette@gmail.com>
2025-12-12 23:09:22 +0200

    create a way to query chembl

--- src/activity_data_chembl.py
+++ src/activity_data_chembl.py
@@ -1,5 +1,10 @@
 
 import requests
+import urllib
+import os
+import pickle
+import json
+from time import sleep
 from cell_lines_cellosaurus import cellosaurus_database_search
 
 
@@ -32,37 +37,14 @@ def chembl_id_from_cellosaurus(base_dir):
                 if db_name in ("ChEMBL", "ChEMBL-Cells"):
                     cell_chembl_id = xref.get("accession")
                     if cell_chembl_id:
-                        #chembl_links[primary_accession] = (cell_chembl_id, identifier_value)
                         chembl_links[cell_chembl_id] = identifier_value
                         break 
+    print(f'Found {len(chembl_links)} cell line chembl IDs')
     return chembl_links
 
-def get_cell_ids(chembl_ids):
-    #cell line can be in assay_organism, assay_cell_type, assay_tissue?, cell_chembl_id
-    #official connection through variant_id = cell_id
-    cell_base_url = "https://www.ebi.ac.uk/chembl/api/data/activity"
-    for cell_line in chembl_ids.keys():
-        params = {'cell_chembl_id': cell_line,'format': 'json', 'limit': 100}
-        try:
-            while cell_base_url:
-                    session = requests.Session()
-                    cell_response = session.get(cell_base_url, params=params, timeout=60)
-                    cell_response.raise_for_status()
-                    cell_json = cell_response.json()
-
-                    assay_chembl_ids.extend(a["assay_chembl_id"] for a in cell_json.get("assays", []))
-                    url = assay_json.get("page_meta", {}).get("next")
-                    if url:
-                        if url.startswith("/"):
-                            url = urllib.parse.urljoin("https://www.ebi.ac.uk/chembl/api/data/assay", url)
-                    else:
-                        url = None
-                    sleep(0.1)
-        except requests.exceptions.RequestException as e:
-            print(f"  > Error fetching assays for {chembl_ids[cell_line]: {e}}")
+def create_query(chembl_ids):
+    or_query = " OR ".join(f'{cid}' for cid in chembl_ids)
+    query_str = f'(_metadata.assay_data.cell_chembl_id:({or_query}) AND (standard_relation:=)'
+    return query_str
 
 
-#assay connected with activities through doc_id of assay to assay_id of activities
-def get_activity_data(base_dir):
-    chembl_ids = chembl_id_from_cellosaurus(base_dir)
-    assay_cell_ids = get_cell_ids(chembl_ids)
