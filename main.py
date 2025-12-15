94a3d091a913faca371e3776b7e38b14a72dc6bb
Lisette <pihor.lisette@gmail.com>
2025-12-15 21:35:28 +0200

    changes in main

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import pandas as pd
 from src.get_activity_data_chembl import create_query
-
+from src.data_analysis import analyze_data, get_important
+from src.info_from_description import extract_info_from_description
 
 def main():
-    print(create_query('/home/lisettepihor/Documents/Kool/Magistritöö/Magistritoo'))
+    create_query()
+    analyze_data()
+
+    
+    df = get_important()
+    df_new = extract_info_from_description(df)
+    print(df_new.head())
 
 if __name__ == "__main__":
     main()
