a55267f339f6030d1372e4d03983068bd92a9dd0
Lisette <pihor.lisette@gmail.com>
2026-03-31 13:11:56 +0300

    erinevad kaustad erinevatele tunnuste genereerimisalgoritmidele

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import os
 import random
 import numpy as np
 import torch
-from src.andmete_tootlus import jaota_andmestik, kombo_koos_tunnustega
+from andmete_tootlus import jaota_andmestik, kombo_koos_tunnustega
 from src.otsustusmets import otsustusmets, ennusta, optimeeri_mets
 from src.graafikud import jaotus_hist
-from otsustusmets_analuus import ApplicabilityDomain, tunnus_vs_tunnus, tunnus_vs_y, influence_plot, shap_summary
+from src.otsustusmets_analuus import tunnus_vs_tunnus, tunnus_vs_y, influence_plot, shap_summary
 import joblib
 import pandas as pd
 
@@ -26,24 +26,3 @@ from src.chembl_aktiivuse_andmed import loo_otsing
 #cellosaurus database search -> chembl id cellosaurusest -> ChEMBL otsing -> andmed ainult vajalik -> 
 #leia info kirjeldusest -> andmed duplikaatideta -> parimad kombinatsioonid -> kombo tunnustega
 
-andmed_0 = kombo_koos_tunnustega(0)
-X_treening, y_treening, X_test, y_test = jaota_andmestik(andmed_0, 0, jarjestatud=True)
-parimad_tunnused = otsustusmets(X_treening, y_treening, X_test, y_test, 0, 'tunnused_optimeeritud', ['TPSA', 'SlogP_VSA8', 'BertzCT', 'VSA_EState4', 'AvgIpc'])
-
-mets = joblib.load(os.path.join(os.getcwd(), f'andmed/kombo_nr_0/mudelid/jarjestatud_viimaste_tunnuste_eemaldamine_6_tunnust_otsustusmets.joblib'))
-parimad_tunnused = ['AvgIpc', 'TPSA', 'SMR_VSA3', 'SlogP_VSA8', 'SPS', 'VSA_EState1']
-
-tunnus_vs_tunnus(X_treening, parimad_tunnused, 'andmed/kombo_nr_0/graafikud/jarjestatud_viimaste_tunnuste_eemaldamine_6_tunnust_otsustusmets')
-tunnus_vs_y(X_treening, y_treening, parimad_tunnused, 'andmed/kombo_nr_0/graafikud/jarjestatud_viimaste_tunnuste_eemaldamine_6_tunnust_otsustusmets')
-influence_plot(X_treening.drop('Molecule ChEMBL ID', axis=1),y_treening['pChEMBL Value'], 
-               'andmed/kombo_nr_0/graafikud/jarjestatud_viimaste_tunnuste_eemaldamine_6_tunnust_otsustusmets')
-shap_summary(mets, X_treening[parimad_tunnused], 
-             'andmed/kombo_nr_0/graafikud/jarjestatud_viimaste_tunnuste_eemaldamine_6_tunnust_otsustusmets')
-
-CNS_andmed = pd.read_csv('andmed/Molport_CNS_Focused_Library.csv', sep=',')
-ad = ApplicabilityDomain()
-ad.fit(X_treening[parimad_tunnused], y_treening['pChEMBL Value'], mets)
-molekulide_ad = ad.check_ad(CNS_andmed['SMILES Canonical'], mets, parimad_tunnused, 
-                            'andmed/kombo_nr_0/graafikud/jarjestatud_viimaste_tunnuste_eemaldamine_6_tunnust_otsustusmets')
-
-ennustatud = ennusta(mets, CNS_andmed['SMILES Canonical'], parimad_tunnused, 0, 'jarjestatud', 'viimaste_tunnuste_eemaldamine_6_tunnust_otsustusmets')
--- src/otsustusmets_analuus.py
+++ src/otsustusmets_analuus.py
@@ -55,6 +55,8 @@ def influence_plot(X, y, fail):
     fail = os.path.join(os.getcwd(), f'{fail}_influence_plot.png')
     if os.path.exists(fail):
         return  
+    X = X.set_index('Molecule ChEMBL ID')
+    y.index = X.index
     X_with_const = sm.add_constant(X)
     results = sm.OLS(y, X_with_const).fit()
     fig, ax = plt.subplots(figsize=(12, 8))
@@ -76,69 +78,7 @@ def shap_summary(mudel, X, fail):
     plt.savefig(fail, dpi=300)
     plt.close()
 
-import numpy as np
-import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.neighbors import NearestNeighbors
-from scipy.spatial.distance import cdist
-import matplotlib.pyplot as plt
-
-class ApplicabilityDomain:
-    def __init__(self):
-        self.h_star = None
-        self.X_train = None
-        self.XTX_inv = None
-        self.train_knn_dist = None
-        self.train_vars = None
-        self.nn = None
-
-    def fit(self, X, y, rf_mudel):
-        self.X_train = np.array(X)
-        n, p = self.X_train.shape
-        self.h_star = (3 * p) / n
-        XTX = self.X_train.T @ self.X_train
-        self.XTX_inv = np.linalg.pinv(XTX) 
-        
-        self.nn = NearestNeighbors(n_neighbors=self.k, metric='euclidean')
-        self.nn.fit(self.X_train)
-        distances, _ = self.nn.kneighbors(self.X_train)
-        self.train_knn_dist_limit = np.percentile(distances[:, -1], 95) # 95th percentile as cutoff
-        
-        # 3. Ensemble Variance (Uncertainty)
-        # Calculate variance across all trees for training data
-        tree_preds = np.array([tree.predict(self.X_train) for tree in rf_mudel.estimators_])
-        self.train_vars = np.var(tree_preds, axis=0)
-        self.var_limit = np.percentile(self.train_vars, 95)
-
-    def calculate_leverage(self, X_new):
-        # h_i = x_i^T * (X^T * X)^-1 * x_i
-        leverages = np.array([x @ self.XTX_inv @ x.T for x in X_new])
-        return leverages
-
-    def check_ad(self, X_new, rf_model):
-        X_new = np.array(X_new)
-        
-        # Compute individual metrics
-        leverages = self.calculate_leverage(X_new)
-        knn_dist, _ = self.nn.kneighbors(X_new)
-        knn_dist = knn_dist[:, -1]
-        
-        tree_preds = np.array([tree.predict(X_new) for tree in rf_model.estimators_])
-        variances = np.var(tree_preds, axis=0)
-        
-        # Combine into flags (True = In Domain)
-        lev_flag = leverages <= self.h_star
-        knn_flag = knn_dist <= self.train_knn_dist_limit
-        var_flag = variances <= self.var_limit
-        
-        # Final decision: In-Domain if all (or most) criteria are met
-        combined_flag = lev_flag & knn_flag & var_flag
-        
-        results = pd.DataFrame({
-            'leverage': leverages,
-            'knn_dist': knn_dist,
-            'variance': variances,
-            'in_domain': combined_flag
-        })
-        
-        return results
+def applicability_domain(mudel, X_treening, ennustatud):
+    ad_sees = []
+    
+    return ad_sees
